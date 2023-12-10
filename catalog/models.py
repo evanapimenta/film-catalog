904ef34c07107d237c09cf0246f3f21b82fb9205
Evana Pimenta Michilotti <evana.pimenta@hotmail.com>
2023-12-10 12:53:35 -0300

    Create model 'Catalog', 'Movie', 'Genre'

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,51 @@
 from django.db import models
+from django.utils.translation import gettext as _
 
 # Create your models here.
+class Genre(models.Model):
+
+    name = models.CharField(max_length=50)
+
+    class Meta:
+        verbose_name = _("Genre")
+        verbose_name_plural = _("Genres")
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse("Genre_detail", kwargs={"pk": self.pk})
+
+
+
+class Movie(models.Model):
+
+    title = models.CharField(max_length=100)
+    year = models.IntegerField()
+    genre = models.OneToOneField(Genre, on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name = _("Movie")
+        verbose_name_plural = _("Movies")
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse("Movie_detail", kwargs={"pk": self.pk})
+
+
+
+class Catalog(models.Model):
+
+    
+
+    class Meta:
+        verbose_name = _("Catalog")
+        verbose_name_plural = _("Catalogs")
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse("Catalog_detail", kwargs={"pk": self.pk})
