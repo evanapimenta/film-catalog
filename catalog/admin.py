59380e5bc96a08b4200bc09eee6df11450e060fc
Evana Pimenta Michilotti <evana.pimenta@hotmail.com>
2023-12-29 14:29:10 -0300

    Revert "Add "TV Show" model"
    This reverts commit 9ecb59a2eb851e305ad5eb586162657c75c5abac.

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import Movie, Show, Genre
+from .models import Movie, Genre
 from easy_select2 import select2_modelform
 
 # Register your models here.
+
 
 @admin.register(Movie)
 class MovieAdmin(admin.ModelAdmin):
@@ -10,10 +11,6 @@ class MovieAdmin(admin.ModelAdmin):
     form = select2_modelform(Movie)
     ordering = ('id',)
 
-@admin.register(Show)
-class ShowAdmin(admin.ModelAdmin):
-    '''Admin View for Show'''
-    ordering = ('id',)
 
 @admin.register(Genre)
 class GenreAdmin(admin.ModelAdmin):
