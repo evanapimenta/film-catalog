0cb2a6b31595ed6c38d013d7ba00c38b12d490ee
Evana Pimenta Michilotti <evana.pimenta@hotmail.com>
2023-12-30 16:53:34 -0300

    Add "TV Show" model

--- catalog/forms.py
+++ catalog/forms.py
@@ -10,3 +10,11 @@ class MovieForm(forms.Form):
     movie_genres = forms.ModelMultipleChoiceField(queryset=Genre.objects.all(), required=False, widget=Select2Multiple(attrs={'data-width': '100%'}))
     movie_synopsis = forms.CharField(widget=forms.Textarea, label="Movie Synopsis")
     movie_poster = forms.ImageField(required=False)
+
+
+class ShowForm(forms.Form):
+    show_title = forms.CharField(label="Show title", max_length=100)
+    show_year = forms.DateField(label="Year of release")
+    show_genres = forms.ModelMultipleChoiceField(queryset=Genre.objects.all(), required=False, widget=Select2Multiple(attrs={'data-width': '100%'}))
+    show_synopsis = forms.CharField(widget=forms.Textarea, label="Show Synopsis", max_length=, required=False)
+    show_poster = forms.ImageField(required=False)
