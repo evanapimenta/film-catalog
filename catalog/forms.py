35a00e90be141dc320ed0d6bc095ce8df6c14678
Evana Pimenta Michilotti <evana.pimenta@hotmail.com>
2024-01-02 12:14:46 -0300

    feat(i18n): Add translations for all labels/names.

--- catalog/forms.py
+++ catalog/forms.py
@@ -1,20 +1,21 @@
 from django import forms
+from django.utils.translation import gettext_lazy as _
 
 from easy_select2.widgets import Select2, Select2Multiple
 
 from catalog.models import Movie, Genre
 
 class MovieForm(forms.Form):
-    movie_title = forms.CharField(label="Movie title", max_length=100)
-    movie_year = forms.DateField(label="Year of release")
-    movie_genres = forms.ModelMultipleChoiceField(queryset=Genre.objects.all(), required=False, widget=Select2Multiple(attrs={'data-width': '100%'}))
-    movie_synopsis = forms.CharField(widget=forms.Textarea, label="Movie Synopsis")
-    movie_poster = forms.ImageField(required=False)
+    movie_title = forms.CharField(label=_("Title"), max_length=100)
+    movie_year = forms.DateField(label=_("Release date"))
+    movie_genres = forms.ModelMultipleChoiceField(label=_("Genres"), queryset=Genre.objects.all(), required=False, widget=Select2Multiple(attrs={'data-width': '100%'}))
+    movie_synopsis = forms.CharField(widget=forms.Textarea, label=_("Synopsis"))
+    movie_poster = forms.ImageField(label=_("Poster"), required=False)
 
 
 class ShowForm(forms.Form):
-    show_title = forms.CharField(label="Show title", max_length=100)
-    show_year = forms.DateField(label="Year of release")
-    show_genres = forms.ModelMultipleChoiceField(queryset=Genre.objects.all(), required=False, widget=Select2Multiple(attrs={'data-width': '100%'}))
-    show_synopsis = forms.CharField(widget=forms.Textarea, label="Show Synopsis", max_length=None, required=False)
-    show_poster = forms.ImageField(required=False)
+    show_title = forms.CharField(label=_("Show title"), max_length=100)
+    show_year = forms.DateField(label=_("Release date"))
+    show_genres = forms.ModelMultipleChoiceField(label=_("Genres"), queryset=Genre.objects.all(), required=False, widget=Select2Multiple(attrs={'data-width': '100%'}))
+    show_synopsis = forms.CharField(widget=forms.Textarea, label=_("Synopsis"), max_length=None, required=False)
+    show_poster = forms.ImageField(label=_("Poster"), required=False)
