0386dba761c1ac7016611a553dcd3bd9e38bccba
Evana Pimenta Michilotti <evana.pimenta@hotmail.com>
2024-01-15 13:57:12 -0300

    UI updates

--- core/settings.py
+++ core/settings.py
@@ -121,7 +121,7 @@ LOCALE_PATHS = (os.path.join(BASE_DIR, 'locale'), )
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, '')]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
--- static/js/base.js
+++ static/js/base.js
@@ -1,18 +1,22 @@
 document.addEventListener('DOMContentLoaded', function() {
-    var dropdownItems = document.querySelectorAll(".dropdown-menu .dropdown-item.text-light");
+   var dropdownItems = document.querySelectorAll(".dropdown-menu .dropdown-item.text-light");
   
-    dropdownItems.forEach(function(dropdownItem) {
-       dropdownItem.addEventListener("click", function() {
-          dropdownItems.forEach(function(item) {
-             item.classList.remove("fw-bold");
-             item.classList.remove("text-dark");
-             item.style.backgroundImage = "";
+   dropdownItems.forEach(function(dropdownItem) {
+      dropdownItem.addEventListener("click", function() {
+         dropdownItems.forEach(function(item) {
+            item.classList.remove("fw-bold");
+            item.classList.remove("text-dark");
+            item.style.backgroundImage = "";
+            item.style.transition = "ease-in 0.2s";
 
-            });
-          this.classList.add("fw-bold");
-          this.classList.add("text-dark");
-          this.style.backgroundImage = "linear-gradient(45deg, #e5bbce, #646897)";
-       });
-    });
- });
- 
+         });
+         this.classList.add("fw-bold");
+         this.classList.add("text-dark");
+         this.style.backgroundImage = "linear-gradient(45deg, #e5bbce, #646897)";
+         this.style.transition = "ease-in 0.2s"
+      });
+   });
+
+
+
+});
